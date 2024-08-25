e901f0684f904bd4668df186820691b438cfe35b
ganjinghao2000@gmail.com <ganjinghao2000@gmail.com>
2024-08-26 04:50:30 +0800

    feat: order list for debug

--- src/main/java/com/example/ordersystem/controllers/OrderController.java
+++ src/main/java/com/example/ordersystem/controllers/OrderController.java
@@ -1,10 +1,14 @@
 package com.example.ordersystem.controllers;
 
 import com.example.ordersystem.entities.Order;
+import com.example.ordersystem.entities.Product;
+import com.example.ordersystem.repositories.OrderRepository;
 import com.example.ordersystem.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
@@ -13,9 +17,12 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     @PostMapping("/create")
-    public ResponseEntity<Order> createOrder(@RequestParam Long userId, @RequestParam String productCode, @RequestParam int quantity) {
-        Order order = orderService.createOrder(userId, productCode, quantity);
+    public ResponseEntity<Order> createOrder(@RequestParam String productCode, @RequestParam int quantity) {
+        Order order = orderService.createOrder(productCode, quantity);
         return ResponseEntity.ok(order);
     }
 
@@ -31,9 +38,9 @@ public class OrderController {
         return ResponseEntity.ok(order);
     }
 
-    @PostMapping("/cancel")
-    public ResponseEntity<Void> batchProcessOrderCancellation() {
-        orderService.batchProcessOrderCancellation();
-        return ResponseEntity.ok().build();
+    @GetMapping
+    public ResponseEntity<List<Order>> getAllOrder() {
+        List<Order> orders = orderRepository.findAll();
+        return ResponseEntity.ok(orders);
     }
 }
